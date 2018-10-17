769c2ff7a4bfccec3f69c4058fe67025b6b90548
Marco Haase <github@marco-haase.de>
2018-10-17 11:56:43 +0200

    added first aid kit

--- modules/Items.js
+++ modules/Items.js
@@ -1,5 +1,6 @@
 module.exports = {
     XANAX : {name: 'Xanax', itemId: 206, price: 900000},
     BOX_OF_TISSUES : {name: 'Box of Tissues', itemId: 403, price: 1000},
-    MP3_PLAYER : {name: 'MP3 Player', itemId: 42, price: 1000}
+    MP3_PLAYER : {name: 'MP3 Player', itemId: 42, price: 1000},
+    MP3_PLAYER : {name: 'First Aid Kit', itemId: 67, price: 35000}
 };
