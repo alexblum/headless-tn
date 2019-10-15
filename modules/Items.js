6e031a48cd67ee29c7e2c380e31a803739fda60e
harcomaase <github@marco-haase.de>
2019-10-15 14:08:12 +0200

    fixed constant name

--- modules/Items.js
+++ modules/Items.js
@@ -2,5 +2,5 @@ module.exports = {
     XANAX : {name: 'Xanax', itemId: 206, price: 900000},
     BOX_OF_TISSUES : {name: 'Box of Tissues', itemId: 403, price: 1000},
     MP3_PLAYER : {name: 'MP3 Player', itemId: 42, price: 1000},
-    MP3_PLAYER : {name: 'First Aid Kit', itemId: 67, price: 35000}
+    FIRST_AID_KIT : {name: 'First Aid Kit', itemId: 67, price: 35000}
 };
