a460ab86ddb431a88a85af769981f5219fbc531c
harcomaase <github@marco-haase.de>
2019-10-15 14:20:39 +0200

    added hacking of the bank

--- modules/crimes.js
+++ modules/crimes.js
@@ -1,5 +1,6 @@
 module.exports = {
     SEARCH_FOR_CASH : {name: 'Search the train station', nerve: 2, selectors: ['Search for cash', 'Search the train station']},
     ARSON_WAREHOUSE : {name: 'Arson Warehouse', nerve: 2, selectors: ['Arson', 'Warehouse']},
-    ARSON_WAREHOUSE : {name: 'Plant Stealth virus', nerve: 2, selectors: ['Plant a computer virus', 'Stealth virus']}
+    ARSON_WAREHOUSE : {name: 'Plant Stealth virus', nerve: 2, selectors: ['Plant a computer virus', 'Stealth virus']},
+    HACKING_BANK : {name: 'Hack the bank', nerve: 18, selectors: ['Hacking', 'Hack into a Bank Mainframe']}
 };
