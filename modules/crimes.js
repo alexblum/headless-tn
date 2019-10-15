610ebd872f3f13e6b237970aa46510c989f3ffdf
harcomaase <github@marco-haase.de>
2019-10-15 13:58:06 +0200

    fixed the name of the stealth virus crime

--- modules/crimes.js
+++ modules/crimes.js
@@ -1,5 +1,5 @@
 module.exports = {
     SEARCH_FOR_CASH : {name: 'Search the train station', nerve: 2, selectors: ['Search for cash', 'Search the train station']},
     ARSON_WAREHOUSE : {name: 'Arson Warehouse', nerve: 2, selectors: ['Arson', 'Warehouse']},
-    ARSON_WAREHOUSE : {name: 'Plant Stealth virus', nerve: 2, selectors: ['Plant a computer virus', 'Stealth virus']}
+    VIRUS_STEALTH : {name: 'Plant Stealth virus', nerve: 2, selectors: ['Plant a computer virus', 'Stealth virus']}
 };
