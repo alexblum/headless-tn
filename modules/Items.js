ed44465e5f8e57afe1372a645fef882c5816ccfc
harcomaase <github@marco-haase.de>
2020-10-25 07:37:05 +0100

    added vicodin to item list

--- modules/Items.js
+++ modules/Items.js
@@ -1,5 +1,6 @@
 module.exports = {
     XANAX : {name: 'Xanax', itemId: 206, price: 900000},
+    VICODIN : {name: 'Vicodin', itemId: 205, price: 3350},
     BOX_OF_TISSUES : {name: 'Box of Tissues', itemId: 403, price: 1000},
     MP3_PLAYER : {name: 'MP3 Player', itemId: 42, price: 1000},
     FIRST_AID_KIT : {name: 'First Aid Kit', itemId: 67, price: 35000}
