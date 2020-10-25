5d01a6454b0491d782e4af04d4f53bcc59ae748c
harcomaase <github@marco-haase.de>
2020-10-25 07:40:30 +0100

    added donator pack!

--- modules/Items.js
+++ modules/Items.js
@@ -2,5 +2,6 @@ module.exports = {
     XANAX : {name: 'Xanax', itemId: 206, price: 900000},
     BOX_OF_TISSUES : {name: 'Box of Tissues', itemId: 403, price: 1000},
     MP3_PLAYER : {name: 'MP3 Player', itemId: 42, price: 1000},
-    FIRST_AID_KIT : {name: 'First Aid Kit', itemId: 67, price: 35000}
+    FIRST_AID_KIT : {name: 'First Aid Kit', itemId: 67, price: 35000},
+    DONATOR_PACK : {name: 'Donator Pack', itemId: 283, price: 24500000}
 };
